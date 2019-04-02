18dd4c7cacb9a8faf3554e796553e5c3a885bf11
Jakub N <ktosspl@gmail.com>
2019-04-02 18:50:18 +0000

    gfh

--- resources/lib/plugin.py
+++ resources/lib/plugin.py
@@ -10,15 +10,19 @@ from xbmcplugin import addDirectoryItem, endOfDirectory
 import urlparse
 import xbmc
 import main
+import qbit
 ADDON = xbmcaddon.Addon()
 logger = logging.getLogger(ADDON.getAddonInfo('id'))
 kodilogging.config()
 plugin = routing.Plugin()
 
+qbitIP='192.168.0.101:8080'
 
 @plugin.route('/')
 def index():
     addDirectoryItem(plugin.handle, plugin.url_for(show_category), ListItem("Add torrent (search)"), True)
+    addDirectoryItem(plugin.handle, plugin.url_for(qbitIP), ListItem("Change IP of qbittorrent client"), True)
+    addDirectoryItem(plugin.handle, plugin.url_for(qbitmenu), ListItem("Browse torrents"), True)
     endOfDirectory(plugin.handle)
 
 @plugin.route('/category')
@@ -26,7 +30,7 @@ def show_category():
 	keyboard = xbmc.Keyboard('', 'Search something')
 	keyboard.doModal()
 	input_text = keyboard.getText()
-	xbmc.executebuiltin('Notification(Wpisales:,'+str(input_text)+',5000,/script.hellow.world.png)')
+	# xbmc.executebuiltin('Notification(Wpisales:,'+str(input_text)+',5000,/script.hellow.world.png)')
 	names_and_sources = main.get_items("https://torrentz2.eu/search?f=" + str(input_text))
 	i=2
 	for x in names_and_sources:
@@ -34,7 +38,10 @@ def show_category():
 		i+=1
 	endOfDirectory(plugin.handle)
 
-
+@plugin.route('/qbitmenu')
+def qbitmenu():
+	addDirectoryItem(plugin.handle, "", ListItem("jestes zjebany"), True)
+	endOfDirectory(plugin.handle)
 
 def run():
     plugin.run()
--- resources/lib/qbit.py
+++ resources/lib/qbit.py
@@ -1,4 +1,19 @@
 from qbittorrent import *
+import json
+magnet="magnet:?xt=urn:btih:E2E457B2E77128CD20FAFD0837BBDB9A4D543578&dn=Solo+A+Star+Wars+Story+%282018%29+%5BBluRay%5D+%281080p%29&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337%2Fannounce&tr=udp%3A%2F%2Fexplodie.org%3A6969"
+try:
+	qb = Client('http://192.168.0.101:8080')
+except:
+	print("error kurwa")
 
-qb = Client('http://192.168.0.101:8080')
 qb.login('admin', 'q1w2e3')
+
+qb.download_from_link(magnet)
+alltorrents = qb.torrents()
+tor_hash=""
+for torrent in alltorrents:
+	print(torrent['magnet_uri'])
+	print("elo " + magnet)
+	if(torrent['magnet_uri']==magnet):
+		tor_hash=torrent['hash']
+print(tor_hash)
