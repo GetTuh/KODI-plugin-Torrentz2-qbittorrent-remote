e8995c91e55608799d83cdc340c95f52ffd65604
Daniel Modzelewski <daniel.modzelewski5@gmail.com>
2019-04-02 22:17:38 +0200

    plplp

--- resources/lib/main.py
+++ resources/lib/main.py
@@ -18,32 +18,25 @@ def get_items(item_searched):
             data += parsing.parse_names(dl)
     return data
 
-
-def addresses():
-    address_list = [];
-    for x in searched_sites:
-        pattern = re.match("^http.+", x)
-        if pattern:
-            address_list.append(pattern.group())
-    return address_list
+getmagnet = "https://torrentz2.eu/e2e457b2e77128cd20fafd0837bbdb9a4d543578";
 
 
 def get_torrent_site(addresses):
     try:
-        for x in addresses:
-            data = ()
-            raw_html = conn.simple_get(x)
-            soup = BeautifulSoup(raw_html, 'html.parser')
-            adblock = 1
-            for dl in soup.find_all('a',attrs={"href":re.compile('http')}):  # parsowanie wynikow
-                if adblock != 0:  # dwa pierwsze wyniki to zawsze reklamy
-                    adblock -= 1
-                else:
-                   print(dl)
+        data = ()
+        raw_html = conn.simple_get(addresses)
+        soup = BeautifulSoup(raw_html, 'html.parser')
+        adblock = 1
+        for dl in soup.find_all('a', attrs={"href": re.compile('http')}):  # parsowanie wynikow
+            if adblock != 0:  # dwa pierwsze wyniki to zawsze reklamy
+                adblock -= 1
+            else:
+                print(dl)
+
     except TypeError:
         pass
 
 if __name__ == "__main__":
     searched_sites = get_items(item_searched)
-    addresses = addresses()
-    get_torrent_site(addresses)
+
+    get_torrent_site(getmagnet)
