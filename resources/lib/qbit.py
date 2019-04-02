1cfa16495b2cf6ab99c79048348ae8f94e8107b1
Jakub N <ktosspl@gmail.com>
2019-04-02 19:23:32 +0000

    qbitpy add, delete

--- resources/lib/qbit.py
+++ resources/lib/qbit.py
@@ -11,6 +11,10 @@ qb.login('admin', 'q1w2e3')
 
 def add_torrent(magnet):
 	qb.download_from_link(magnet)
+	tor_hash = magnet_to_hash(magnet)
+	qb.toggle_sequential_download(tor_hash)
+
+def magnet_to_hash(magnet): #Searches for torrent using magnet, returns hash
 	magnet_url=magnet.lower()
 	magnet_url=re.match(".*?&dn=", magnet_url).group(0)
 	alltorrents = qb.torrents()
@@ -20,6 +24,12 @@ def add_torrent(magnet):
 		torrent_magnet = re.match(".*?&dn=", torrent_magnet).group(0)
 		if(torrent_magnet==magnet_url):
 			tor_hash=torrent['hash']
-	qb.toggle_sequential_download(tor_hash)
+	return tor_hash
 
-add_torrent(magnet)
+# add_torrent(magnet)
+
+def delete_torrent_pernament(magnet):
+	tor_hash = magnet_to_hash(magnet)
+	qb.delete_permanently(tor_hash)
+
+delete_torrent_pernament(magnet)
