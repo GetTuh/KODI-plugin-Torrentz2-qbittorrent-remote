d16f989fc01916bdfd30e35c282651a4d06a8fec
Jakub N <ktosspl@gmail.com>
2019-04-03 09:32:19 +0000

    odjebane wykrywanie magnetow ez

--- resources/lib/site_parsing.py
+++ resources/lib/site_parsing.py
@@ -1,7 +1,7 @@
 import re
 from bs4 import BeautifulSoup
 import conn
-
+import time
 def link_to_magnet(site):
     raw_html = conn.simple_get(site)
     soup = BeautifulSoup(raw_html, 'html.parser')
@@ -12,11 +12,26 @@ def link_to_magnet(site):
     	if(adblock==0):			#pierwszy wynik to reklama wiec go omijam
     		adblock+=1
     	else:
-    		print(siteLink.get('href'))
+    		siteLink=siteLink.get('href')
+    		print("trying " + siteLink)
+    		magnet = siteLink_to_magnet(siteLink)
+    		if(magnet!=None):
+    			return(magnet)
+    		print("No magnet found")
+
 
 def siteLink_to_magnet(siteLink):
-	pass
+    raw_html = conn.simple_get(siteLink)
+    try:
+	    soup = BeautifulSoup(raw_html, 'html.parser')
+	    mag_re=re.compile("href=\"magnet:\?")
+	    magnet = soup.findAll('a')
+	    for a in magnet:
+	    	if re.findall("href=\"magnet:\?",str(a.encode('utf-8'))):
+	    		return(a.get('href'))
+    except:
+    	return None
 
+# print(siteLink_to_magnet("https://1337x.to/torrent/3223403/Solo-A-Star-Wars-Story-2018-BluRay-1080p-YTS-YIFY/"))
 
-
-link_to_magnet("https://torrentz2.eu/e2e457b2e77128cd20fafd0837bbdb9a4d543578")
+print(link_to_magnet("https://torrentz2.eu/e2e457b2e77128cd20fafd0837bbdb9a4d543578"))
